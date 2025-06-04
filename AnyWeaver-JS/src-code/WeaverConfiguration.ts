f355974a5865b54bcf494db1dd78290ffb10205b
João Bispo <jbispo@fe.up.pt>
2025-06-04 16:51:12 +0100

    [Anyweaver-JS] Fixes anyweaver launching

--- AnyWeaver-JS/src-code/WeaverConfiguration.ts
+++ AnyWeaver-JS/src-code/WeaverConfiguration.ts
@@ -3,12 +3,12 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 export const weaverConfig: WeaverConfiguration = {
-    weaverName: "anyweaver",
-    weaverPrettyName: "AnyWeaver",
-    jarPath: path.join(
-        path.dirname(path.dirname(fileURLToPath(import.meta.url))),
-        "./java-binaries/"
-    ),
-    javaWeaverQualifiedName: "pt.up.fe.specs.anycompiler.weaver.AnyWeaver",
-    importForSideEffects: ["anyweaver-js/api/Joinpoints.js"],
+  weaverName: "anyweaver",
+  weaverPrettyName: "AnyWeaver",
+  jarPath: path.join(
+    path.dirname(path.dirname(fileURLToPath(import.meta.url))),
+    "./java-binaries/"
+  ),
+  javaWeaverQualifiedName: "pt.up.fe.specs.anycompiler.weaver.AnyWeaver",
+  importForSideEffects: ["@specs-feup/anyweaver/api/Joinpoints.js"],
 };
