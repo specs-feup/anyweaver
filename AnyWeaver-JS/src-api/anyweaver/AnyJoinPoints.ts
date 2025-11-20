a603484f1a480e410c46167a525a82011187968c
João Bispo <jbispo@fe.up.pt>
2025-11-20 17:48:16 +0000

    [AnyWeaver-JS] Adds class AnyJoinPoints to be able to create new join points in AnyWeaver

---
+++ AnyWeaver-JS/src-api/anyweaver/AnyJoinPoints.ts
@@ -0,0 +1,28 @@
+import {
+  wrapJoinPoint,
+} from "@specs-feup/lara/api/LaraJoinPoint.js";
+
+import * as Joinpoints from "../Joinpoints.js";
+import AnyJavaTypes from "./AnyJavaTypes.js";
+
+/**
+ * Utility methods related with the creation of new join points.
+ *
+ */
+export default class AnyWeaverJoinPoints {
+  static toJoinPoint(node: any): Joinpoints.Joinpoint {
+    return wrapJoinPoint(AnyJavaTypes.AnyJoinPoints.createFromLara(node));
+  }
+
+  /**
+   * Creates a new, empty Any join point.
+   *
+   * @param kind
+   * @returns
+   */
+  static any(kind: string): Joinpoints.Any {
+    return wrapJoinPoint(AnyJavaTypes.AnyJpFactory.anyNode(kind));
+  }
+
+  
+}
