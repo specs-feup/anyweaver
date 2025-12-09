422620b6e6fe7541f5743bbe93cbde0939c62c37
João Bispo <jbispo@fe.up.pt>
2025-12-09 14:21:47 +0000

    [AnyWeaver-JS] Using lara v3.0.14, adds custom mapper for AnyWeaver

--- AnyWeaver-JS/src-code/index.ts
+++ AnyWeaver-JS/src-code/index.ts
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 import WeaverLauncher from "@specs-feup/lara/code/WeaverLauncher.js";
+import { registerJoinpointMapperFunction } from "@specs-feup/lara/api/LaraJoinPoint.js";
 import { weaverConfig } from "./WeaverConfiguration.js";
+import { Any, App } from "../api/Joinpoints.js";
+
+// Register jp mapper that always returns Any expect for App
+registerJoinpointMapperFunction((jpTypename: string) => {
+  if (jpTypename === "Joinpoint 'app'") {
+    return App;
+  }
+
+  return Any;
+});
 
 const weaverLauncher = new WeaverLauncher(weaverConfig);
 
